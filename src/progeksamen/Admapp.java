fedd0cecbc312e5159ed6702cb2b8faa33e0b46a
Daniel Kviteberg Larssen <voje@live.no>
2019-05-06 09:32:18 +0200

    FIrst Committ
    Added the Admapp and Spillerapp

---
+++ src/progeksamen/Admapp.java
@@ -0,0 +1,35 @@
+/*
+ * Main
+ */
+package progeksamen;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Admapp extends Application implements Constants {
+    
+    /**
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        
+        
+        // Setter her opp scene, for å ha en måte å lukke applikasjonen enklere på.
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, SCREEN_HEIGHT, SCREEN_WIDTH);
+        primaryStage.setTitle("Application!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+         
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/progeksamen/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progeksamen;
+
+/**
+ * 
+ */
+public interface Constants {
+    
+    // The width and height of the scene.
+    public int SCREEN_HEIGHT = 600;
+    public int SCREEN_WIDTH  = 800;
+
+}

5dd0ab1fff904d99c322fa8eb9a2326d2bfa2f4a
Markus Fenes <markusfenes@gmail.com>
2019-05-07 12:04:41 +0200

    Added a text dialog box
    This will be used for giving names to the players and tournaments

--- src/progeksamen/Tournament.java
+++ src/progeksamen/Tournament.java
@@ -8,9 +8,17 @@ public class Tournament {
     List<Player> players = new ArrayList();
     List<Game> games = new ArrayList();
     List<Result> results = new ArrayList();
+    
+    String name;
 
     //Player player;
-    public Tournament(List players, List games, List results) {
+    public Tournament(String name){
+        this.name = name;
+      
+    }
+    
+    public Tournament(String name,List players, List games, List results) {
+        this.name = name;
         this.players = players;
         this.games = games;
         this.results = results;
@@ -19,7 +27,7 @@ public class Tournament {
 
     @Override
     public String toString() {
-        return this.players + " Games : " + this.games + " Winners : " + this.results;
+        return "Name : " + this.name + " " + this.players + " Games : " + this.games + " Winners : " + this.results;
     }
 
 }
